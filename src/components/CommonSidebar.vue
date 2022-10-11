925f09cd41b5c80b6416b1da1e0aca98c12832f0
Maciej Romanowski <neipreis22@gmail.com>
2022-10-11 20:57:23 +0200

    created common sidebar

---
+++ src/components/CommonSidebar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps({
+  show: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  left: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  right: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="sidebar"
+    v-show="show"
+    :class="{
+      'sidebar--left': left,
+      'sidebar--right': right,
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.sidebar {
+  position: absolute;
+  top: 56px;
+  height: calc(100vh - 56px);
+  width: 240px;
+  background-color: #262626;
+  padding: 12px;
+
+  &--left {
+    left: 0;
+  }
+
+  &--right {
+    right: 0;
+  }
+}
+</style>
