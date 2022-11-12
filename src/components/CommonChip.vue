4fb480b3a8175b7e98c8fa674359efae573fe751
Maciej Romanowski <neipreis22@gmail.com>
2022-11-12 23:04:42 +0100

    created common chip component

---
+++ src/components/CommonChip.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="common-chip"
+    :class="{ 'common-chip--selected': selected }"
+    tabindex="0"
+    @keypress="selected = !selected"
+    @click="selected = !selected"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const selected = ref(false);
+</script>
+
+<style scoped lang="scss">
+.common-chip {
+  border-radius: 4px;
+  padding: 8px 12px;
+  flex: 0 1 auto;
+  width: fit-content;
+  font-size: 14px;
+  font-weight: 400;
+  font-family: Arial, sans-serif;
+  background-color: rgba($color: #fff, $alpha: 0.12);
+  transition: all ease-in-out 0.2s;
+  border: 1px solid transparent;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover,
+  &:focus-visible {
+    outline: none;
+    border-color: #9fa8da;
+  }
+
+  &--selected {
+    background-color: #9fa8da;
+    color: #262626;
+
+    &:hover,
+    &:focus-visible {
+      border-color: #fff;
+    }
+  }
+}
+</style>
