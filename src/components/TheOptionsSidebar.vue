8124c75b56068001ccb18d13f144981c4f1bfa42
Maciej Romanowski <neipreis22@gmail.com>
2022-11-13 13:27:21 +0100

    - added array with available handbooks - added chip group - added prop with selected items

--- src/components/TheOptionsSidebar.vue
+++ src/components/TheOptionsSidebar.vue
@@ -5,6 +5,12 @@
         <template #label>Druid level</template>
       </common-input>
 
+      <common-chip-group
+        v-model:modelValue="selectedItems"
+        :items="items"
+        :column="true"
+      />
+
       <common-checkbox id="moon-druid">
         <template #label>Moon druid</template>
       </common-checkbox>
@@ -13,9 +19,30 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import type { IChipGroupItem } from '../types/IChipGroupItem';
+
 import CommonSidebar from './CommonSidebar.vue';
 import CommonInput from './CommonInput.vue';
 import CommonCheckbox from './CommonCheckbox.vue';
+import CommonChipGroup from './CommonChipGroup.vue';
+
+const items: IChipGroupItem[] = [
+  {
+    text: 'Monster Manual',
+    value: 'MM',
+  },
+  {
+    text: `Volo's Guide to Monsters`,
+    value: 'GtM',
+  },
+  {
+    text: `Mordenkainen's Tome of Foes`,
+    value: 'ToF',
+  },
+];
+
+const selectedItems = ref(['MM', 'GtM', 'ToF']);
 </script>
 
 <style scoped lang="scss">
