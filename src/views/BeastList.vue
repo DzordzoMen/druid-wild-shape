7b92bbd6e5461422bd207c74a3a0d4ca7b0567c6
Maciej Romanowski <neipreis22@gmail.com>
2022-11-15 23:05:03 +0100

    created beast list view

---
+++ src/views/BeastList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="beast-list">
+    <beast-list-item
+      v-for="(beast, index) in list"
+      :key="index"
+      :beast="beast"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IBeast } from '@/types/IBeast';
+
+import monsterManualBeasts from '@/assets/books/monster-manual.json';
+
+import BeastListItem from '@/components/BeastListItem.vue';
+
+const list = monsterManualBeasts as IBeast[];
+</script>
+
+<style scoped lang="scss">
+.beast-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px;
+}
+</style>
