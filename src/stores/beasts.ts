aab118491cc5d075ea59648ad07ce5e8c127f832
Maciej Romanowski <neipreis22@gmail.com>
2022-11-18 21:52:03 +0100

    updated beasts module

--- src/stores/beasts.ts
+++ src/stores/beasts.ts
@@ -1,20 +1,37 @@
-// import { computed } from 'vue';
-import { defineStore } from 'pinia';
 import type { IBeast } from '@/types/IBeast';
+import { defineStore } from 'pinia';
 
 import mmBeasts from '@/assets/books/monster-manual.json';
 import tomeOfFoes from '@/assets/books/tome-of-foes.json';
 import voloGuideToMonsters from '@/assets/books/volos-guide-to-monsters.json';
 
-export const useCounterStore = defineStore('beasts', () => {
-  const monsterManualBeasts = mmBeasts as IBeast[];
-  const tomeOfFoesBeasts = tomeOfFoes as IBeast[];
-  const voloBeasts = voloGuideToMonsters as IBeast[];
+interface State {
+  monsterManualBeasts: IBeast[];
+  tomeOfFoesBeasts: IBeast[];
+  voloBeasts: IBeast[];
+}
 
-  // const doubleCount = computed(() => count.value * 2);
-  // function increment() {
-  //   count.value++;
-  // }
+export const useBeastsStore = defineStore('beasts', {
+  state: (): State => ({
+    monsterManualBeasts: mmBeasts as IBeast[],
+    tomeOfFoesBeasts: tomeOfFoes as IBeast[],
+    voloBeasts: voloGuideToMonsters as IBeast[],
+  }),
+  getters: {
+    availableBeastsFromSelectedBooks(state): IBeast[] {
+      const { monsterManualBeasts, tomeOfFoesBeasts, voloBeasts } = state;
+      const usedHandbooks = localStorage.getItem('used-handbooks');
+      const result: IBeast[] = [];
 
-  return { monsterManualBeasts, tomeOfFoesBeasts, voloBeasts };
+      if (usedHandbooks?.includes('MM')) result.push(...monsterManualBeasts);
+      if (usedHandbooks?.includes('GtM')) result.push(...voloBeasts);
+      if (usedHandbooks?.includes('ToF')) result.push(...tomeOfFoesBeasts);
+
+      return result;
+    },
+    // TODO filter
+    availableBeasts(): IBeast[] {
+      return this.availableBeastsFromSelectedBooks;
+    },
+  },
 });
