11a7aac0cc33893b6a71945a91d0badec9802f90
Maciej Romanowski <neipreis22@gmail.com>
2022-11-18 22:00:21 +0100

    updated beasts module to use druid options module

--- src/stores/beasts.ts
+++ src/stores/beasts.ts
@@ -1,6 +1,8 @@
 import type { IBeast } from '@/types/IBeast';
 import { defineStore } from 'pinia';
 
+import { useDruidOptionsStore } from './druidOptions';
+
 import mmBeasts from '@/assets/books/monster-manual.json';
 import tomeOfFoes from '@/assets/books/tome-of-foes.json';
 import voloGuideToMonsters from '@/assets/books/volos-guide-to-monsters.json';
@@ -20,7 +22,9 @@ export const useBeastsStore = defineStore('beasts', {
   getters: {
     availableBeastsFromSelectedBooks(state): IBeast[] {
       const { monsterManualBeasts, tomeOfFoesBeasts, voloBeasts } = state;
-      const usedHandbooks = localStorage.getItem('used-handbooks');
+      const druidOptionsModule = useDruidOptionsStore();
+
+      const usedHandbooks = druidOptionsModule.selectedBooks;
       const result: IBeast[] = [];
 
       if (usedHandbooks?.includes('MM')) result.push(...monsterManualBeasts);
