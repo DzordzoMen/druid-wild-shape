daf59cd0e98d5a6f6f8638361d41db77caf8c509
Maciej Romanowski <neipreis22@gmail.com>
2022-11-18 21:51:38 +0100

    added druid options pinia module

---
+++ src/stores/druidOptions.ts
@@ -0,0 +1,27 @@
+import { useStorage, type RemovableRef } from '@vueuse/core';
+import { defineStore } from 'pinia';
+
+interface State {
+  druidLevel: RemovableRef<string>;
+  selectedBooks: RemovableRef<string[]>;
+  moonDruid: RemovableRef<boolean>;
+}
+
+export const useDruidOptionsStore = defineStore('druid-options', {
+  state: (): State => ({
+    druidLevel: useStorage('druid-level', ''),
+    selectedBooks: useStorage('used-handbooks', ['MM', 'GtM', 'ToF']),
+    moonDruid: useStorage('moon-druid', true),
+  }),
+  actions: {
+    setDruidLevel(value: string): void {
+      this.druidLevel = value;
+    },
+    setSelectedBooks(value: string[]): void {
+      this.selectedBooks = value;
+    },
+    setMoonDruid(value: boolean): void {
+      this.moonDruid = value;
+    },
+  },
+});
