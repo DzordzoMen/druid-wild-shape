9516b51154e803e4592804970cf1a860f8bd2b9f
Maciej Romanowski <neipreis22@gmail.com>
2022-12-01 22:57:18 +0100

    updated beastInfo page

--- src/views/BeastInfo.vue
+++ src/views/BeastInfo.vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <div class="beast-info">
+    <div>{{ beastInfo?.name }}</div>
+    <br />
+    <div>{{ beastInfo }}</div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useBeastsStore } from '@/stores/beasts';
 
-<style scoped lang="scss"></style>
+const route = useRoute();
+const beastModule = useBeastsStore();
+
+const beastName: string = route.params.beastName as string;
+
+const beastInfo = computed(() => {
+  return beastModule.getBeastDataByName(beastName);
+});
+</script>
+
+<style scoped lang="scss">
+.beast-info {
+  max-width: 1020px;
+  margin: auto;
+  color: #fff;
+}
+</style>
