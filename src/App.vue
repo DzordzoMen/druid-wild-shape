552895042e058542c8107545f3d8f89e11cab8c2
Maciej Romanowski <neipreis22@gmail.com>
2022-12-04 20:39:52 +0100

    - updated header styles - added place for app name - added icons to header

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@ import { RouterView } from 'vue-router';
 
 import FiltersSidebar from './components/TheFiltersSidebar.vue';
 import OptionsSidebar from './components/TheOptionsSidebar.vue';
+import IconClose from './components/icons/IconClose.vue';
+import IconFilter from './components/icons/IconFilter.vue';
+import iconOptions from './components/icons/IconOptions.vue';
 
 const showLeftSidebar = ref(true);
 const showRightSidebar = ref(true);
@@ -19,15 +22,19 @@ const showOptionsSidebar = computed(() => {
   return route.name === 'BeastInfo' ? false : showRightSidebar.value;
 });
 
-function test(): void {
+function toggleLeftSidebar(): void {
   showLeftSidebar.value = !showLeftSidebar.value;
 }
 </script>
 
 <template>
   <header>
-    <button @click="test()">Left</button>
-    <button @click="showRightSidebar = !showRightSidebar">Right</button>
+    <icon-filter @click="toggleLeftSidebar()" />
+
+    <div>Wild Shape</div>
+
+    <icon-options v-if="!showRightSidebar" @click="showRightSidebar = true" />
+    <icon-close v-else @click="showRightSidebar = false" />
   </header>
 
   <filters-sidebar :show="showFiltersSidebar" />
@@ -48,7 +55,24 @@ header {
   height: 56px;
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.9), 0 0px 2px rgba(0, 0, 0, 0.9) !important;
   background-color: #18181b;
+  padding: 12px;
   z-index: 8;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  gap: 10px;
+
+  & > svg {
+    cursor: pointer;
+  }
+
+  & > div {
+    color: #fff;
+    font-size: 24px;
+    font-weight: 600;
+  }
 }
 
 // #7668cb
