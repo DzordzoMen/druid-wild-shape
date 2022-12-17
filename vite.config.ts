7bc4eca963ac1c9694f048770b4cdc8f15390975
Maciej Romanowski <neipreis22@gmail.com>
2022-12-17 14:00:44 +0100

    - added data for PWA in vite config file - added manifest data

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true, // TODO remove
+      },
+      manifest: {
+        name: 'Druid Wild Shape',
+        short_name: 'DWS',
+        display: 'standalone',
+        start_url: '/',
+        orientation: 'portrait-primary',
+        description: 'Dungeons and Dragons 5 - Druid Wild Shape',
+        theme_color: '#18181b',
+        background_color: '#121212',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
