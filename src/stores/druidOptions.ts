50c7f79d0ffa118613aeeaa4865e2b20f8a4fc2d
Maciej Romanowski <neipreis22@gmail.com>
2023-02-10 19:55:04 +0100

    updated default value for moon druid

--- src/stores/druidOptions.ts
+++ src/stores/druidOptions.ts
@@ -11,7 +11,7 @@ export const useDruidOptionsStore = defineStore('druid-options', {
   state: (): State => ({
     druidLevel: useStorage('druid-level', ''),
     selectedBooks: useStorage('used-handbooks', ['MM', 'GtM', 'MotM']),
-    moonDruid: useStorage('moon-druid', true),
+    moonDruid: useStorage('moon-druid', false),
   }),
   actions: {
     setDruidLevel(value: string | number): void {
